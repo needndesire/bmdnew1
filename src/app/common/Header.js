e503e2317ecb4fc15ab1aa775d599ed2c859de28
Neeshu <neeshuhaldaur@gmail.com>
2024-12-21 15:01:36 +0530

    hel

--- src/app/common/Header.js
+++ src/app/common/Header.js
@@ -6,7 +6,7 @@ import { CiSearch } from "react-icons/ci";
 export default function Header() {
   return (
    <>
-   <header className='font-sans border-b-[1px] bg-[#fffefe] shadow-sm'>
+   <header className='font-sans border-b-[1px] bg-[#fffefe] shadow-lg'>
     <ul className='container m-auto flex justify-between items-center py-2 px-1'>
         <li className='flex items-center gap-1 '>
             <img src='https://clipart-library.com/image_gallery/n1163695.jpg' className='h-[50px] rounded-lg'/>
--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
    <>
-   <section className="bg-slate-300">
+   <section className="bg-[white] h-[100vh]">
    <h1>
-    neeshu kumar
+    neeshu kumar Rajput
    </h1>
    </section>
    
