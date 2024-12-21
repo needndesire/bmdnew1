d327cc924fb70ad20574d415fbd7846dd9a7dbbf
Neeshu <neeshuhaldaur@gmail.com>
2024-12-21 15:59:31 +0530

    hel

---
+++ src/app/HomeComponent/Card1.js
@@ -0,0 +1,95 @@
+"use client"
+import Link from 'next/link'
+
+
+export default function Card1() {
+  
+        return (
+        <>
+            <section className='bg-white text-black '>
+                <div className='container m-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 p-1'>
+                <div className='h-[275px]  md:h-[300px] lg:h-[350px] shadow-md rounded-[4px]'>
+                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRUJefeY1mcZ17Uyy9NmcAd5vhuMTCrD7n_Q&s' className='h-[55%] w-full rounded-[4px]' />
+                        <h2 className='text-[20px] font-[700] px-1'>Healers Hospital 
+                        </h2>
+                        <p className='px-1 text-[10px] lg:text-[15px] md:text-[13px]'>
+                        is a leading multispecialty hospital located in Bijnor. Renowned for its exceptional patient-centered care
+                        </p>
+                        <Link href='/Hospitaldetail'>
+                         <button type='submit' className='border block m-auto text-[16px] lg:text-[18px]  bg-[#2c4cdb] text-white rounded-md lg:h-[40px] h-[25px] sm:h-[30px] mt-2 w-[70%] sm:w-[65%]' >SEE MORE </button></Link>
+                      
+                     
+                    </div>
+                    <div className='h-[275px]  md:h-[300px] lg:h-[350px] shadow-md rounded-[4px]'>
+                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRUJefeY1mcZ17Uyy9NmcAd5vhuMTCrD7n_Q&s' className='h-[55%] w-full rounded-[4px]' />
+                        <h2 className='text-[20px] font-[700] px-1'>Healers Hospital 
+                        </h2>
+                        <p className='px-1 text-[10px] lg:text-[15px] md:text-[13px]'>
+                        is a leading multispecialty hospital located in Bijnor. Renowned for its exceptional patient-centered care
+                        </p>
+                        <Link href='/Hospitaldetail'>
+                         <button type='submit' className='border block m-auto text-[16px] lg:text-[18px]  bg-[#2c4cdb] text-white rounded-md lg:h-[40px] h-[25px] sm:h-[30px] mt-2 w-[70%] sm:w-[65%]' >SEE MORE </button></Link>
+                      
+                     
+                    </div>
+                    <div className='h-[275px]  md:h-[300px] lg:h-[350px] shadow-md rounded-[4px]'>
+                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRUJefeY1mcZ17Uyy9NmcAd5vhuMTCrD7n_Q&s' className='h-[55%] w-full rounded-[4px]' />
+                        <h2 className='text-[20px] font-[700] px-1'>Healers Hospital 
+                        </h2>
+                        <p className='px-1 text-[10px] lg:text-[15px] md:text-[13px]'>
+                        is a leading multispecialty hospital located in Bijnor. Renowned for its exceptional patient-centered care
+                        </p>
+                        <Link href='/Hospitaldetail'>
+                         <button type='submit' className='border block m-auto text-[16px] lg:text-[18px]  bg-[#2c4cdb] text-white rounded-md lg:h-[40px] h-[25px] sm:h-[30px] mt-2 w-[70%] sm:w-[65%]' >SEE MORE </button></Link>
+                      
+                     
+                    </div>
+                    <div className='h-[275px]  md:h-[300px] lg:h-[350px] shadow-md rounded-[4px]'>
+                        <img src='https://static.vecteezy.com/system/resources/thumbnails/000/219/529/small/medical-background.jpg' className='h-[55%] w-full rounded-[4px]' />
+                        <h2 className='text-[20px] font-[700] px-1'>Healers Hospital 
+                        </h2>
+                        <p className='px-1 text-[10px] lg:text-[15px] md:text-[13px]'>
+                        is a leading multispecialty hospital located in Bijnor. Renowned for its exceptional patient-centered care
+                        </p>
+                        <Link href='/Hospitaldetail'>
+                         <button type='submit' className='border block m-auto text-[16px] lg:text-[18px]  bg-[#2c4cdb] text-white rounded-md lg:h-[40px] h-[25px] sm:h-[30px] mt-2 w-[70%] sm:w-[65%]   '>SEE MORE </button></Link>
+                      
+                     
+                    </div>
+                    <div className='h-[275px]      md:h-[300px]  lg:h-[350px]  rounded-[4px] shadow-md'>
+                        3
+                    </div>
+                    <div className='h-[275px]      md:h-[300px]  lg:h-[350px]  rounded-[4px] shadow-md'>
+                        3
+                    </div>
+                    <div className='h-[275px]      md:h-[300px]  lg:h-[350px]  rounded-[4px] shadow-md'>
+                        3
+                    </div>
+                    <div className='h-[275px]      md:h-[300px]  lg:h-[350px]  rounded-[4px] shadow-md'>
+                        3
+                    </div>
+                    <div className='h-[275px]      md:h-[300px]  lg:h-[350px]  rounded-[4px] shadow-md'>
+                        3
+                    </div>
+                    <div className='h-[275px]      md:h-[300px]  lg:h-[350px]  rounded-[4px] shadow-md'>
+                        3
+                    </div>
+                    <div className='h-[275px]      md:h-[300px]  lg:h-[350px]  rounded-[4px] shadow-md'>
+                        3
+                    </div>
+                    <div className='h-[275px]      md:h-[300px]  lg:h-[350px]  rounded-[4px] shadow-md'>
+                        3
+                    </div>
+                    <div className='h-[275px]      md:h-[300px]  lg:h-[350px]  rounded-[4px] shadow-md'>
+                        3
+                    </div>
+                    <div className='h-[275px]      md:h-[300px]  lg:h-[350px]  rounded-[4px] shadow-md'>
+                        3
+                    </div>
+
+                    
+                </div>
+            </section>
+        </>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,10 @@
 import Image from "next/image";
+import Card1 from "./HomeComponent/Card1";
 
 export default function Home() {
   return (
    <>
-   <section className="bg-[white] h-[100vh]">
-   <h1>
-    neeshu kumar Rajput
-   </h1>
-   </section>
-   
+   <Card1/>
    </>
   );
 }
