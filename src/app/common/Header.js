44b96919a8ccb025bd17877354c9a16f546667a8
Neeshu <neeshuhaldaur@gmail.com>
2024-12-21 14:50:25 +0530

    hel

--- src/app/common/Header.js
+++ src/app/common/Header.js
@@ -8,17 +8,18 @@ export default function Header() {
    <>
    <header className='font-sans border-b-[1px] shadow-sm'>
     <ul className='container m-auto flex justify-between items-center py-2 '>
-        <li className='flex items-center gap-1 border'>
+        <li className='flex items-center gap-1 '>
             <img src='https://clipart-library.com/image_gallery/n1163695.jpg' className='h-[50px] rounded-lg'/>
             <p className='uppercase font-[700] text-[#575454]'>book my doctor</p>
         </li>
         <li className='md:hidden'>
        
         </li>
-        <li className='border w-[50%] h-[40px] hidden md:block'>
+        <li className=' w-[50%] h-[40px] hidden md:block relative'>
             <input type='text' className='border rounded-md w-[100%] h-[100%] placeholder:ps-2' placeholder='Search...' />
+            <CiSearch className='absolute top-3 left-[90%]'/>
         </li>
-        <li className='md:flex  justify-center items-center gap-4 text-[16px] border hidden '>
+        <li className='md:flex  justify-center items-center gap-4 text-[16px] hidden '>
             <div className='text-[#474646]'><button>Sign In</button></div>
             <div className='rounded-md p-2 bg-[black] text-white'>  <button>Login In</button></div>
                   
