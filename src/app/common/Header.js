5c87369290d2e20213d55db4c71b89edf387c5c0
neeshu <neeshuhaldaur@gmail.com>
2024-12-22 23:06:28 +0530

    h

--- src/app/common/Header.js
+++ src/app/common/Header.js
@@ -6,21 +6,22 @@ import Link from 'next/link';
 
 export default function Header() {
     let [menu, setmenu] = useState(false)
+    let [search,setsearch]=useState(false)
     return (
         <>
             <section className={`h-[100vh] w-[100%] bg-[rgba(0,0,0,.5)] fixed z-[9999] ${menu ? `translate-x-[0%]` : `translate-x-[-100%]`}`}>
                 <p className='absolute text-2xl border rounded-lg px-2 right-0 m-5' onClick={() => setmenu(!menu)}>&times;</p>
-                <div className=' grid grid-cols-[60%_auto] border'>
+                <div className=' grid grid-cols-[50%_auto] border'>
                     <div>
                         
                     </div>
                     <div className='pt-[50px] flex flex-col gap-1 px-5 h-[100vh] bg-[white]'>
                         <div className='text-[#474646] p-2'><button>Sign In</button></div>
                         <div className='rounded-md p-2 bg-[black] text-white'>  <button>Login In</button></div>
-                    </div>
+                    </div> 
                 </div>
             </section>
-            <header className='font-sans border-b-[1px] sticky top-0 bg[#fffefe] shadow-lg'>
+            <header className='font-sans border-b-[1px] sticky top-0  shadow-lg bg-[#fdfcfc]'>
                 <ul className='container m-auto flex justify-between items-center py-2 px-1'>
                     <Link href='/'><li className='flex items-center gap-1 cursor-pointer'>
                         <img src='https://clipart-library.com/image_gallery/n1163695.jpg' className='h-[50px] rounded-lg' />
@@ -40,12 +41,21 @@ export default function Header() {
 
                     </li>
                     <li className='md:hidden flex gap-[50px] text-[black]'>
-                        <CiSearch />
+                        <CiSearch onClick={()=>setsearch(!search)} />
 
                         <RiMenuFill onClick={() => setmenu(!menu)} />
 
                     </li>
+                    
+                  
+                
                 </ul>
+                <div className={`h-[50px]  justify-center items-center ${search ?`flex`:`hidden`}`}>
+                    <div className=' w-[80%] h-[40px] relative'>
+                        <input type='text' className='border rounded-md w-[100%] h-[100%] placeholder:ps-2' placeholder='Search...' />
+                        <CiSearch className='absolute top-3 left-[90%]' />
+                        </div>
+                    </div>
             </header>
         </>
     )
