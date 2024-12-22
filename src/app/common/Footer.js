9712190752601e0764ac335968c6e464350522b1
neeshu <neeshuhaldaur@gmail.com>
2024-12-23 00:50:47 +0530

    h

--- src/app/common/Footer.js
+++ src/app/common/Footer.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-      <section>
+      <section className=' '>
         
 
-<footer className="bg-white  m-4 dark:bg-gray-800">
+<footer className="bg-white ">
     <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" className="hover:underline">Book My Doctor™</a>. All Rights Reserved.
     </span>
