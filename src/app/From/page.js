9a46cf97e0968e17f7479e2480c0f6cc6e4163ee
neeshu <neeshuhaldaur@gmail.com>
2024-12-23 19:05:10 +0530

    h

--- src/app/From/page.js
+++ src/app/From/page.js
@@ -11,7 +11,7 @@ export default function page() {
         <form>
           <div className="grid gap-6 mb-6 md:grid-cols-2">
             <div>
-              <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+              <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Patiant name</label>
               <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
             </div>
             <div>
