59b2e696a921a38df8c70e3f19bff3f9f1d35346
neeshu <neeshuhaldaur@gmail.com>
2025-01-10 22:56:52 +0530

    k

--- src/app/Model/Model.js
+++ src/app/Model/Model.js
@@ -11,19 +11,19 @@ export default function Model({ setshowModel }) {
 
     
     let googleLogin = () => {
-        // const auth = getAuth(app);
-        // signInWithPopup(auth, provider)
-        //     .then((result) => {
-        //         // This gives you a Google Access Token. You can use it to access the Google API.
-        //         const credential = GoogleAuthProvider.credentialFromResult(result);
-        //         const token = credential.accessToken;
-        //         // The signed-in user info.
-        //         const user = result.user;
-        //         // IdP data available using getAdditionalUserInfo(result)
-        //         // ...
-        //         console.log(token,user)
+        const auth = getAuth(app);
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+                console.log(token,user)
 
-        //     })
+            })
         console.log("neeshu kumar")
     }
     return (
